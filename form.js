a65afe83ec63e3e1e2cb2ae5a38355797da79dd6
L.Carquet <laetitiacarquet@gmail.com>
2019-12-26 10:23:53 +0100

    initial commit

---
+++ form.js
@@ -0,0 +1,71 @@
+//TODO: I need to be able to edit the comment box while selecting other things.
+//TODO: be able to change the gender after being selected???
+//TODO: clear button
+//TODO: counting words? character? when they enter the comment box
+//TODO: check the conjugation for they/ or the other around add s if he or she?
+//TODO: BBDD for one year, recording score of student for to compare and comment not to repeat.
+
+
+
+//return the gender value
+const genderButton = document.querySelector("#genderBtn");
+//text area where all the comments go
+const commentBox = document.getElementById("comments");
+//button that triggers the finetuning of the comment
+const FINISH = document.querySelector("#finishBt");
+//area where the all form is with several buttons to add content
+const FORM = document.querySelector("FORM");
+
+//A. buttons that add comments onto the text field
+FORM.addEventListener("click", clickButtonAdd);
+//B. finetune the final textarea
+FINISH.addEventListener("click", finishText);
+//button that check the gender
+genderButton.addEventListener("click",checkGender);
+
+
+//1.click on any button to retrieve the comment linked to it
+function clickButtonAdd(event){
+
+  if (event.target.nodeName == "BUTTON") {
+      addComments(event.target.parentNode.id);
+    }
+}
+//2.take what's inside the option drop down and add it in the comment box without erasing the previous comment.
+function addComments(e) {
+  //dropdown selection
+ let strUser =  document.getElementById("select" + e).value;
+console.log(strUser);
+  commentBox.append(strUser + " ");
+  //here launch a function that count the charcates/words?
+}
+
+//replace pronouns
+let replacement = "";
+// step 1/2 replace pronoun: check student's gender and return a pronoun
+function checkGender(){
+  const ele = document.getElementsByName('gender');
+    for(let i = 0; i < ele.length; i++) { 
+        if(ele[i].checked) 
+          return replacement =  ele[i].value;   
+    } 
+}
+
+//step 2/2 replace pronoun: insert pronoun 
+function finishText(){
+  checkGender();
+  //check if nothing has been entered you need to leave with (std) where should this be?
+  if(replacement == ""){
+    //TODO: launch a message 
+    alert("please, select a gender");
+    console.log(replacement);
+  } else{
+    let replace = commentBox.innerHTML.replace("(std)", replacement);
+  commentBox.innerHTML = replace;
+  }
+}
+
+
+//to handle the text area counting characters
+//https://eloquentjavascript.net/15_event.html
+      
